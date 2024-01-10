618570a787e2efae2934a9129a11602603be2060
arjun <arjun3458@gmail.com>
2024-01-10 21:20:07 +0530

    catalouge service

--- catalogue.sh
+++ catalogue.sh
@@ -33,7 +33,7 @@ systemctl start catalogue &>>/tmp/roboshop.log
 
 
 echo -e "\e[35mCopy mongodb repo file\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 
 echo -e "\e[35mInstalling mongodb client\e[0m"
