9eef2967d9e1300d5e7a0cd57e4af0381a8f8175
arjun <arjun3458@gmail.com>
2024-01-11 11:33:58 +0530

    roboshop service

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,9 @@
 echo -e "\e[34m Disable mysql default password\e[0m"
 yum module disable mysql -y &>>/tmp/roboshop.log
 
+echo -e "\e[34m Copy mysql repo file\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
 echo -e "\e[34mInstall my sql community server\e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 
