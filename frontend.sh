35d1e08cab48b1f5a7be74197b1407dd7172c96b
arjun <arjun3458@gmail.com>
2024-01-11 18:36:02 +0530

    Payment service

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[35mInstalling nginx server\e[0m"
-yum install nginx -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+echo -e "${color}Installing nginx server${nocolor}"
+yum install nginx -y &>>/tmp/roboshop.log &>>$log_file
 
-echo -e "\e[35mRemoving app content\e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+echo -e "${color}Removing app content${nocolor}"
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
-echo -e "\e[35mDownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+echo -e "${color}Downloading frontend content${nocolor}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
-echo -e "\e[33mExtracting frontend content\e[0m"
+echo -e "${color}Extracting frontend content${nocolor}"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>$log_file
 
-echo -e "\e[33mUpdate frontend congif file\e[0m"
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+echo -e "${color}Update frontend congif file${nocolor}"
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
 
-echo -e "\e[33mStarting nginx server\e[0m"
-systemctl enable nginx &>>/tmp/roboshop.log
-systemctl restart nginx &>>/tmp/roboshop.log
+echo -e "${color}Starting nginx server${nocolor}"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
