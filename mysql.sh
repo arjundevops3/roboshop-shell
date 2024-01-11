3c7643419a608a7456e432ba0e451d3b7e1be561
arjun <arjun3458@gmail.com>
2024-01-11 11:27:03 +0530

    roboshop service

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,12 @@
-echo -e "\e[35mInstalling nginx server\e[0m"
-dnf module disable mysql -y &>>/tmp/roboshop.log
+echo -e "\e[34m Disable mysql default password\e[0m"
+yum module disable mysql -y &>>/tmp/roboshop.log
 
-echo -e "\e[35mInstalling nginx server\e[0m"
-dnf install mysql-community-server -y &>>/tmp/roboshop.log
+echo -e "\e[34mInstall my sql community server\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
 
-echo -e "\e[35mInstalling nginx server\e[0m"
+echo -e "\e[35mStart mysql service\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
 systemctl restart mysqld &>>/tmp/roboshop.log
 
-echo -e "\e[35mInstalling nginx server\e[0m"
+echo -e "\e[35mSetup mysql password\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
-
-echo -e "\e[35mInstalling nginx server\e[0m"
-mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
