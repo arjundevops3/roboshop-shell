c373187c1bbb45e428363aa706865c0b72bd5cf4
arjun <arjun3458@gmail.com>
2024-01-17 22:42:55 +0530

    Payment service

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,3 +1,5 @@
+source common.sh
+
 echo -e "${color}configure Erlang repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 stat_check $?
