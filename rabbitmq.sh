87b9d9c4ef9e40f520e41b7dfee62a1c1b90d144
arjun <arjun3458@gmail.com>
2024-01-17 22:41:29 +0530

    Payment service

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,21 +1,21 @@
-echo -e "$(color)configure Erlang repos$(nocolor)"
+echo -e "${color}configure Erlang repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "$(color)Configure rabbimq repos$(nocolor)"
+echo -e "${color}Configure rabbimq repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "$(color)Install rabbitmq server$(nocolor)"
+echo -e "${color}Install rabbitmq server${nocolor}"
 yum install rabbitmq-server -y &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "$(color)Start rabbitmq service $(nocolor)"
+echo -e "${color}Start rabbitmq service ${nocolor}"
 systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl restart rabbitmq-server &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "$(color) ADDRabbitmq application user$(nocolor)"
+echo -e "${color} ADDRabbitmq application user${nocolor}"
 rabbitmqctl add_user roboshop $1 &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
 stat_check $?
